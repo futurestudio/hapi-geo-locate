00795c2a15b7dca9f57cf79ef3c7204dde68f07c
Marcus Poehls <marcus.poehls@gmail.com>
2016-12-22 15:03:52 +0100

    reformat code due to editorconfig changes

--- test/index.js
+++ test/index.js
@@ -3,43 +3,47 @@ const Code = require('code');
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({port: 3000});
+server.connection({ port: 3000 });
 
 const lab = exports.lab = Lab.script();
+const experiment = lab.experiment;
+const test = lab.test;
 
-lab.experiment('hapi-geo-locate register plugin', function () {
-  lab.before(function (done) {
-    server.register({
-      register: require('../lib/index')
-    }, function (err) {
-      done(err);
+experiment('hapi-geo-locate register plugin', function () {
+
+    lab.before(function (done) {
+        server.register({
+            register: require('../lib/index')
+        }, function (err) {
+            done(err);
+        });
     });
-  });
 
-  lab.test('test if the plugin works without any options', function (done) {
-    const NO_OPTIONS = {
-      path: '/NO_OPTIONS',
-      method: 'GET',
-      handler: function (request, reply) {
-        reply(request.location);
-      }
-    }
+    test('test if the plugin works without any options', function (done) {
 
-    server.route(NO_OPTIONS)
+        const NO_OPTIONS = {
+            path: '/NO_OPTIONS',
+            method: 'GET',
+            handler: function (request, reply) {
+                reply(request.location);
+            }
+        }
 
-    const options = {
-      url: NO_OPTIONS.path,
-      method: NO_OPTIONS.method
-    };
+        server.route(NO_OPTIONS)
 
-    server.inject(options, function (response) {
-      const payload = JSON.parse(response.payload || '{}');
+        const options = {
+            url: NO_OPTIONS.path,
+            method: NO_OPTIONS.method
+        };
 
-      Code.expect(response.statusCode).to.equal(200);
-      Code.expect(Object.keys(payload)).to.contain(['ip']);
+        server.inject(options, function (response) {
+            const payload = JSON.parse(response.payload || '{}');
 
-      done();
+            Code.expect(response.statusCode).to.equal(200);
+            Code.expect(Object.keys(payload)).to.contain([ 'ip' ]);
+
+            done();
+        });
     });
-  });
 
 })
